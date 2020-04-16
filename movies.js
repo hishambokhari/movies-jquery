b1cccb83247e6aa13ba1eacd39a25d0a0a7c49c2
root <hish.b92@gmail.com>
2020-04-16 16:26:26 +0500

    Axios

--- movies.js
+++ movies.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
   $('#searchForm').on('submit', function(e){
-    let searchText = $('#searchText').val()
+    let searchText = $('#searchText').val();
     getMovies(searchText);
     e.preventDefault();
   });
 });
 
 function getMovies(searchText){
-  console.log(searchText);
+  axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=thewdb')
+   .then((response) => {
+      console.log(response);
+    })
+   .catch((err) => {
+     console.log(err);
+   });
 }
